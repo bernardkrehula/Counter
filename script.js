14c171b6df155765d6cdfb49f8a67d5a9d08ca4b
bernardkrehula <krehula.bernard456@gmail.com>
2025-01-29 20:51:27 +0100

    Diffrent way

--- script.js
+++ script.js
@@ -3,46 +3,45 @@ let btn = document.querySelectorAll('.btn button');
 
 let count = 0;
 
-let clickedButton;
+let activeIndex;
 
 function incrementCount() {
-    if(clickedButton === 'increase'){
         count += 1;
-    }
 } 
 function decrementCount() {
-    if(clickedButton === 'decrease'){
         count -= 1;
-    }
 }
 function resetCount() {
-    if(clickedButton === 'reset'){
         count = 0;
-    }
 }
 function getCount() {
     return count;
 }
-
-btn.forEach((element) => {
+console.log(activeIndex);
+btn.forEach((element, index) => {
     element.addEventListener('click', (event) => {
-        clickedButton = event.target.id;
         
-        incrementCount();
-        decrementCount();
-        resetCount();
-        getCount();
-
+        activeIndex = index;
         result.innerHTML = getCount();
         
-        if(count < 0) {
-            result.style.color = 'red';
-        }
-        if(count > 0) {
-            result.style.color = 'green';
-        }
-        if(count === 0) {
-            result.style.color = 'rgb(0, 102, 107)';
-        }
+            if(activeIndex === 2) {
+                incrementCount();
+                if(count >= 0) {
+                    result.style.color = 'green';
+                }
+            }
+            if(activeIndex === 0) {
+                decrementCount();
+                if(count <= 0) {
+                    result.style.color = 'red';
+                }
+            }
+            if(activeIndex === 1){
+                resetCount();
+                if(count === 0) {
+                    result.style.color = 'rgb(0, 102, 107)';
+        
+                }
+            }
     })        
 })
